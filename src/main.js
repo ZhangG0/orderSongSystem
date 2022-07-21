84affc524ab3ac3323b4dc48713d1ee5d0e4035f
ZhangG0 <1073266994@qq.com>
2022-07-21 21:07:48 +0800

    [引入头像标签组件]

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 //引入NutUI
-import { Image, Button, Icon, Form, FormItem, Cell, CellGroup, Checkbox,CheckboxGroup, Table ,BackTop } from '@nutui/nutui';
+import { Image, Button, Icon, Form, FormItem, Cell, CellGroup, Checkbox,CheckboxGroup, Table ,BackTop, Avatar } from '@nutui/nutui';
 // import NutUI from "@nutui/nutui";
 import "@nutui/nutui/dist/style.css";
 // 全局css导入
@@ -31,7 +31,7 @@ app.use(store).use(router)
     .use(ElementPlus, {locale: zhCn,size:'small'})
     .mount('#app');
 
-app.use(Cell).use(CellGroup).use(Icon).use(Table).use(BackTop);
+app.use(Cell).use(CellGroup).use(Icon).use(Table).use(BackTop).use(Avatar);
 app.use(Form).use(FormItem);
 app.use(Button).use(Image);
 app.use(Checkbox).use(CheckboxGroup);
