bf1be21fcab2e92571dcce9c471d7860c7f6b236
ZhangG0 <zhanggzero@qq.com>
2022-10-11 16:37:38 +0800

    【NEW】新增<SingCell>与<SingCellGroup>组件

---
+++ src/components/Sing/SingCell.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container" :class="{round}">
+    <div class="left itemDiv">
+      <span>{{leftMain}}</span>
+      <span v-if="leftSub" class="leftSub"><br/> - {{leftSub}}</span>
+    </div>
+
+    <div class="right itemDiv">
+      {{rightMain}}
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import '@/static/css/commonResponse.scss'
+
+const props = defineProps({
+  leftMain:String,
+  leftSub:String,
+  rightMain:String,
+  round:{
+    type:Boolean,
+    default:false
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.container{
+  width: 100%;
+  padding: 10px;
+  min-height: 60px;
+  border: 1px solid lightgray;
+  margin: 10px;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(255, 255, 255, 0.99);
+}
+.round{
+  border-radius: 18px;
+}
+.left{
+  max-width: 70%;
+  font-size: 17px;
+  margin-left: 5px;
+}
+.right{
+  align-self: center;
+  max-width: 28%;
+}
+.leftSub{
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  font-family: cursive;
+}
+.itemDiv{
+  align-self: center;
+  display: inline-block;
+  margin-right: 5px;
+}
+</style>
---
+++ src/components/Sing/SingCellGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sing-cell-group SingerListDiv">
+    <div class="group-title" :class="{textCenter}">{{groupTitle}}</div>
+    <slot >
+<!--      原则上要SingCell-->
+    </slot>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  groupTitle:String,
+  textCenter:Boolean
+})
+</script>
+
+<style lang="scss" scoped>
+.textCenter{
+  text-align: center;
+}
+.sing-cell-group{
+  border: 1px solid lightgray;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 0.99);
+
+  .group-title{
+    padding: 5px;
+    margin: 5px;
+  }
+
+  :slotted(.container){
+    margin: 0;
+    border: 0;
+    border-bottom: lightgray 1px solid;
+  }
+  :slotted(.container:last-child){
+    border: 0;
+  }
+}
+</style>
