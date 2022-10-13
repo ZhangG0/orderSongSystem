450e21b6069a33423a74983ac84687c25d9b1272
ZhangG0 <zhanggzero@qq.com>
2022-10-13 10:02:48 +0800

    【新增】SingDialog组件封装完毕

--- src/components/Sing/SingDialog.vue
+++ src/components/Sing/SingDialog.vue
@@ -4,31 +4,11 @@
     <nut-dialog
         :title="title"
         v-model:visible="visible"
-        :custom-class="`${isInputDialog} ?'inputDialog':''`"
     >
 
       <!--      content部分-->
-      <slot name="content">
-
-        <div
-            v-if="isInputDialog"
-        >
-          <el-form :model="formData" :rules="rules" :ref="formRef">
-            <el-form-item prop="firstLineLabel">
-              <span class="dialogELInputLabel">{{ firstLineLabel }}</span>
-              <el-input class="dialogELInput" :placeholder="fstPlaceholder" v-model="formData.firstLine" clearable></el-input>
-            </el-form-item>
-            <el-form-item s prop="secondLineLabel">
-              <span class="dialogELInputLabel">{{ secondLineLabel }}</span>
-              <el-input class="dialogELInput" :placeholder="scdPlaceholder" v-model="formData.secondLine" clearable></el-input>
-            </el-form-item>
-          </el-form>
-        </div>
-
-        <div v-else>
-          <div style="line-height: 20px" >{{text}}</div>
-        </div>
-
+      <slot>
+        {{ text }}
       </slot>
 
       <!--      foot部分-->
@@ -37,7 +17,7 @@
           <el-button
               round
               style="width: 42%;padding: 5px"
-              @click="$emit('close')"
+              @click="visible = false"
               v-if="!noCancelBtn"
           >取消</el-button>
           <el-button round style="width: 42%;padding: 5px;color: white" color="#d5432f" @click="!noCancelBtn?clickFunc():$emit('close')">{{ okText }}</el-button>
@@ -68,30 +48,7 @@ import {reactive, ref} from "vue";
     visible:{
       type:Boolean,
       default:false,
-      required:true
     },
-    /**
-     * 1. 会对具有输入框的dialog做视觉优化(增加class)
-     * 2. content部分变为两个输入框
-     */
-    isInputDialog:{
-      type:Boolean,
-      default:false
-    },
-    //content表单
-    formData:{
-      type:Object,
-      default:{
-        firstLine:"",
-        secondLine:""
-      }
-    },
-    formRules:{
-      type:Object,
-      default:{}
-    },
-    formRef:String,
-
     //content文字
     text:String,
     //footer
@@ -107,8 +64,6 @@ import {reactive, ref} from "vue";
       }
     }
   })
-  const temp = ref(props.formRules);
-  const rules = reactive({temp});
 </script>
 
 <style scoped>
